bc7b6eb0f9e1deb37cd59f5223cca668a345d41e
RealET <real3t@gmail.com>
2024-06-19 17:43:31 +0200

    fix : langage plus inclusif (et plus clair)
    cf https://git.spip.net/spip/forum/-/merge_requests/4790/diffs

---
+++ lang/forum_fr.php
@@ -0,0 +1,26 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans https://git.spip.net/spip/forum.git
+
+return [
+	'bouton_radio_enregistrement_obligatoire' => 'Enregistrement obligatoire (les utilisateurs doivent s’abonner en fournissant leur adresse email avant de pouvoir poster des contributions).',
+	'bouton_radio_moderation_priori' => 'Modération a priori (les
+	contributions ne s’affichent publiquement qu’après validation par les
+	administrateurs).',
+	'bouton_radio_publication_immediate' => 'Publication immédiate des messages
+	(les contributions s’affichent dès leur envoi, les administrateurs peuvent
+	les supprimer ensuite).',
+	'forum_info_modere' => 'Ce forum est modéré a priori : votre contribution n’apparaîtra qu’après avoir été validée par un administrateur du site.',
+	'info_config_forums_prive_admin' => 'Un forum réservé aux administrateurs du site :',
+	'info_config_forums_prive_global' => 'Un forum global, ouvert à tous les rédacteurs :',
+	'info_envoi_forum' => 'Envoi des forums aux auteurs des articles',
+	'info_option_email' => 'Lorsqu’un visiteur du site poste un nouveau message dans le forum associé à un article, les auteurs de l’article peuvent être prévenus de ce message par email. Indiquer pour chaque type de forum s’il faut utiliser cette option.',
+	'info_question_visiteur_ajout_document_forum' => 'Si vous souhaitez autoriser les visiteurs à joindre des documents (images, sons…) à leurs messages de forum, indiquez ci-dessous la liste des extensions de documents autorisés pour les forums (ex : gif, jpg, png, mp3).',
+	'item_activer_forum_administrateur' => 'Activer le forum des administrateurs',
+	'item_config_forums_prive_global' => 'Activer le forum des rédacteurs',
+	'item_desactiver_forum_administrateur' => 'Désactiver le forum des administrateurs',
+	'item_non_config_forums_prive_global' => 'Désactiver le forum des rédacteurs',
+	'messages_meme_auteur' => 'Tous les messages de cet auteur',
+	'messages_privadm' => 'Administrateurs',
+	'titre_cadre_forum_administrateur' => 'Forum privé des administrateurs',
+];
