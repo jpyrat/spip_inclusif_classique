70dc5c383bd09d816ef551ed81b180f23b22cbe9
RealET <real3t@gmail.com>
2024-06-19 17:33:31 +0200

    fix : langage plus inclusif (et plus clair)

--- lang/spip_fr.php
+++ lang/spip_fr.php
@@ -6,7 +6,21 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 }
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-
+	'info_contact_developpeur' => 'Veuillez contacter un développeur.',
+	'info_installer_ftp' => 'En tant qu’administrateur, vous pouvez installer (par FTP) des fichiers dans le dossier @upload@ pour ensuite les sélectionner directement ici.',
+	'info_nom_non_utilisateurs_connectes' => 'Votre nom n’apparaît pas dans la liste des utilisateurs connectés.',
+	'info_nom_utilisateurs_connectes' => 'Votre nom apparaît dans la liste des utilisateurs connectés.',
+	'info_symbole_jaune' => 'Le symbole <b>jaune</b> indique une <b>annonce à tous les rédacteurs</b> : modifiable par tous les administrateurs, et visible par tous les rédacteurs.',
+	'info_symbole_vert' => 'Le symbole <b>vert</b> indique les <b>messages échangés avec d’autres utilisateurs</b> du site.',
+	'pass_erreur_code_inconnu' => '<b>Erreur :</b> ce code ne correspond à aucun des visiteurs ayant accès à ce site.',
+	'pass_espace_prive_bla' => 'L’espace privé de ce site est ouvert aux
+		visiteurs, après inscription. Une fois enregistré,
+		vous pourrez consulter les articles en cours de rédaction,
+		proposer des articles et participer à tous les forums.',
+	'pass_forum_bla' => 'Vous avez demandé à intervenir sur un forum
+		réservé aux visiteurs enregistrés.',
 	'texte_actualite_site_1' => 'Quand vous serez familiarisé avec l’interface, vous pourrez cliquer sur « ',
+	'texte_inc_meta_1' => 'Le système a rencontré une erreur lors de l’écriture du fichier <code>@fichier@</code>. Veuillez, en tant qu’administrateur du site,',
+	'titre_image_administrateur' => 'Administrateur',
 
 );
